fe5ea6677795509fb0acadf22e88e37207ba667e
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-10-28 20:16:15 -0300

    Refactor general

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 import loaddata from './modules/formvalidation.js';
 import hidepages from './modules/hidepages.js';
-import { hidemenu, showmenu } from './modules/hidemenu.js';
 
+const homeBtn = document.getElementById('home');
 const portfolioBtn = document.getElementById('portfolio');
 const aboutBtn = document.getElementById('about');
-const contactBtn = document.getElementById('contact');
-const resumeBtn = document.getElementById('resume');
-const myheader = document.getElementsByTagName('header')[0];
 
 window.addEventListener('load', () => {
   portfolioBtn.addEventListener('click', hidepages);
   aboutBtn.addEventListener('click', hidepages);
-  contactBtn.addEventListener('click', hidepages);
-  resumeBtn.addEventListener('click', hidepages);
-  myheader.addEventListener('mouseover', showmenu);
-  document.addEventListener('click', hidemenu);
+  homeBtn.addEventListener('click', hidepages);
 
   loaddata();
 });
--- modules/hidepages.js
+++ modules/hidepages.js
@@ -1,56 +1,36 @@
+const homeDisplay = document.getElementById('homesection');
 const portfolioDisplay = document.getElementById('portfoliosection');
 const aboutDisplay = document.getElementById('aboutsection');
-const contactDisplay = document.getElementById('contactsection');
-const resumeDisplay = document.getElementById('resumesection');
+const homeBtn = document.getElementById('home');
 const portfolioBtn = document.getElementById('portfolio');
 const aboutBtn = document.getElementById('about');
-const contactBtn = document.getElementById('contact');
-const resumeBtn = document.getElementById('resume');
 
 export default (event) => {
   window.scrollTo(0, 0);
   switch (event.target.id) {
-    case 'portfolio':
+    case 'home':
+      if (homeDisplay.classList.contains('hidden')) homeDisplay.classList.remove('hidden');
+      if (!portfolioDisplay.classList.contains('hidden')) portfolioDisplay.classList.add('hidden');
       if (!aboutDisplay.classList.contains('hidden')) aboutDisplay.classList.add('hidden');
-      if (!contactDisplay.classList.contains('hidden')) contactDisplay.classList.add('hidden');
-      if (!resumeDisplay.classList.contains('hidden')) resumeDisplay.classList.add('hidden');
+      if (!homeBtn.classList.contains('rightmenuSelected')) homeBtn.classList.add('rightmenuSelected');
+      if (portfolioBtn.classList.contains('rightmenuSelected')) portfolioBtn.classList.remove('rightmenuSelected');
+      if (aboutBtn.classList.contains('rightmenuSelected')) aboutBtn.classList.remove('rightmenuSelected');
+      break;
+    case 'portfolio':
+      if (!homeDisplay.classList.contains('hidden')) homeDisplay.classList.add('hidden');
       if (portfolioDisplay.classList.contains('hidden')) portfolioDisplay.classList.remove('hidden');
+      if (!aboutDisplay.classList.contains('hidden')) aboutDisplay.classList.add('hidden');
+      if (homeBtn.classList.contains('rightmenuSelected')) homeBtn.classList.remove('rightmenuSelected');
       if (!portfolioBtn.classList.contains('rightmenuSelected')) portfolioBtn.classList.add('rightmenuSelected');
       if (aboutBtn.classList.contains('rightmenuSelected')) aboutBtn.classList.remove('rightmenuSelected');
-      if (contactBtn.classList.contains('rightmenuSelected')) contactBtn.classList.remove('rightmenuSelected');
-      if (resumeBtn.classList.contains('rightmenuSelected')) resumeBtn.classList.remove('rightmenuSelected');
       break;
-
     case 'about':
+      if (!homeDisplay.classList.contains('hidden')) homeDisplay.classList.add('hidden');
       if (!portfolioDisplay.classList.contains('hidden')) portfolioDisplay.classList.add('hidden');
-      if (!contactDisplay.classList.contains('hidden')) contactDisplay.classList.add('hidden');
-      if (!resumeDisplay.classList.contains('hidden')) resumeDisplay.classList.add('hidden');
       if (aboutDisplay.classList.contains('hidden')) aboutDisplay.classList.remove('hidden');
+      if (homeBtn.classList.contains('rightmenuSelected')) homeBtn.classList.remove('rightmenuSelected');
+      if (portfolioBtn.classList.contains('rightmenuSelected')) portfolioBtn.classList.remove('rightmenuSelected');
       if (!aboutBtn.classList.contains('rightmenuSelected')) aboutBtn.classList.add('rightmenuSelected');
-      if (portfolioBtn.classList.contains('rightmenuSelected')) portfolioBtn.classList.remove('rightmenuSelected');
-      if (contactBtn.classList.contains('rightmenuSelected')) contactBtn.classList.remove('rightmenuSelected');
-      if (resumeBtn.classList.contains('rightmenuSelected')) resumeBtn.classList.remove('rightmenuSelected');
-      break;
-
-    case 'contact':
-      if (!portfolioDisplay.classList.contains('hidden')) portfolioDisplay.classList.add('hidden');
-      if (!aboutDisplay.classList.contains('hidden')) aboutDisplay.classList.add('hidden');
-      if (contactDisplay.classList.contains('hidden')) contactDisplay.classList.remove('hidden');
-      if (!resumeDisplay.classList.contains('hidden')) resumeDisplay.classList.add('hidden');
-      if (!contactBtn.classList.contains('rightmenuSelected')) contactBtn.classList.add('rightmenuSelected');
-      if (portfolioBtn.classList.contains('rightmenuSelected')) portfolioBtn.classList.remove('rightmenuSelected');
-      if (aboutBtn.classList.contains('rightmenuSelected')) aboutBtn.classList.remove('rightmenuSelected');
-      if (resumeBtn.classList.contains('rightmenuSelected')) resumeBtn.classList.remove('rightmenuSelected');
-      break;
-    case 'resume':
-      if (!portfolioDisplay.classList.contains('hidden')) portfolioDisplay.classList.add('hidden');
-      if (!aboutDisplay.classList.contains('hidden')) aboutDisplay.classList.add('hidden');
-      if (!contactDisplay.classList.contains('hidden')) contactDisplay.classList.add('hidden');
-      if (resumeDisplay.classList.contains('hidden')) resumeDisplay.classList.remove('hidden');
-      if (!resumeBtn.classList.contains('rightmenuSelected')) resumeBtn.classList.add('rightmenuSelected');
-      if (portfolioBtn.classList.contains('rightmenuSelected')) portfolioBtn.classList.remove('rightmenuSelected');
-      if (aboutBtn.classList.contains('rightmenuSelected')) aboutBtn.classList.remove('rightmenuSelected');
-      if (contactBtn.classList.contains('rightmenuSelected')) contactBtn.classList.remove('rightmenuSelected');
       break;
     default:
       break;
