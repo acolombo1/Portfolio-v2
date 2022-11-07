ddd0906e18f016280cd248e91d75e3913251a4e2
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-11-07 20:55:24 -0300

    Fix mobile non clickable items

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ import showproject from './modules/showproject.js';
 const homeBtn = document.getElementById('home');
 const portfolioBtn = document.getElementById('portfolio');
 const aboutBtn = document.getElementById('about');
-const thumbs = document.getElementsByClassName('thumb');
+const thumb1 = document.getElementById('thumb1');
+const thumb2 = document.getElementById('thumb2');
+const thumb3 = document.getElementById('thumb3');
+const thumb4 = document.getElementById('thumb4');
+const thumb5 = document.getElementById('thumb5');
+const thumb6 = document.getElementById('thumb6');
+const thumb8 = document.getElementById('thumb8');
 
 function scrolltotop() {
   window.scrollTo(0, 0);
@@ -15,10 +21,14 @@ window.addEventListener('load', () => {
   portfolioBtn.addEventListener('click', hidepages);
   aboutBtn.addEventListener('click', hidepages);
   homeBtn.addEventListener('click', hidepages);
-  [...thumbs].forEach((thumb) => {
-    thumb.addEventListener('click', showproject);
-  });
+  thumb1.addEventListener('click', showproject);
+  thumb2.addEventListener('click', showproject);
+  thumb3.addEventListener('click', showproject);
+  thumb4.addEventListener('click', showproject);
+  thumb5.addEventListener('click', showproject);
+  thumb6.addEventListener('click', showproject);
+  thumb8.addEventListener('click', showproject);
 
   loaddata();
-  setTimeout(scrolltotop, 200);
+  setTimeout(scrolltotop, 100);
 });
